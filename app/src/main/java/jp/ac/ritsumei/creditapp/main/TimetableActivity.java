93cf2da3bcb217063ecf1bf8300f65d99fa4e4a7
mii <mii5656develop@gmail.com>
2014-06-08 15:34:35 +0900

    時間割画面の修正(フリッパービューの準備)

--- app/src/main/java/jp/ac/ritsumei/creditapp/main/TimetableActivity.java
+++ app/src/main/java/jp/ac/ritsumei/creditapp/main/TimetableActivity.java
@@ -1,6 +1,50 @@
 package jp.ac.ritsumei.creditapp.main;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TextView;
+
+import jp.ac.ritsumei.creditapp.app.R;
 
 public class TimetableActivity extends Activity {
+
+    private Time currentTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.timetable);
+
+        //日時設定
+        setCurrentTime();
+
+        //TODO データベースから読み込み 曜日
+
+
+        //TODO 週
+
+
+        //TODO 月
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+    protected void setCurrentTime() {
+        currentTime = new Time("asia/Tokyo");
+        currentTime.setToNow();
+        TextView timeText = (TextView) findViewById(R.id.extView1);
+        timeText.setText((currentTime.month + 1) + "月" + currentTime.monthDay + "日");
+    }
+
+
+
 }
